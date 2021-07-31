2af6c9c8e1437ca95f66433ecc62d5cdc09a908e
Marco <marco.wettstein@gmail.com>
2021-07-31 20:44:16 +0200

    only write gitignore if dir contains mod.go file

--- main.go
+++ main.go
@@ -15,7 +15,27 @@ var gitignoreContent string = `*.exe
 *.out
 `
 
+var goModFileName string = "go.mod"
+
 func main() {
+	if shouldWriteGitignore() {
+		writeGitignore()
+	}
+}
+
+func shouldWriteGitignore() bool {
+	// only write .gitingore if we are in a directory containing a go.mod
+	if _, err := os.Stat(goModFileName); err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("Not writing .gitignore since this does not seem to be a go directory")
+			return false
+		}
+	}
+
+	return true
+}
+
+func writeGitignore() {
 	if err := os.WriteFile(".gitignore", []byte(gitignoreContent), 0666); err != nil {
 		log.Fatalf("Could not write .gitignore %v\n", err)
 		return
